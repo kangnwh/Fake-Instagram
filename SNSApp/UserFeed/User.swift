8a90052b52d90e045103efe55cfb8b04f08f7794
raelynlyu <raelynlyu@gmail.com>
2018-10-15 13:44:22 +1100

    user feed modify

---
+++ SNSApp/UserFeed/User.swift
@@ -0,0 +1,33 @@
+//  Created by Raelyn Lyu on 2/10/18.
+//  Copyright © 2018 Raelyn Lyu. All rights reserved.
+//
+import UIKit
+
+struct User
+{
+    var username: String?
+    var profileImage: UIImage?
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
